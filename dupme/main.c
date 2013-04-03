70be51aecae6f34873a76f243b1034bebc14fe82
watson <watson@watson.(none)>
2013-04-03 22:16:51 +0400

    It works

--- dupme/main.c
+++ dupme/main.c
@@ -15,5 +15,41 @@ int getInt(char * s) {
 
 int main (int argc, char * argv[]) {
 	int k = getInt(argv[1]) ;		
-	printf("%i", k);
+	int len = 0;
+	int lastindex, curindex, i;
+	int isWrite = 1;
+	char * buffer;
+	buffer = malloc(k + 1);
+	while (1) {
+         	int r = read(0, buffer + len, k - len + 1);
+		if (r == 0) {
+			free(buffer);
+			return 0;
+		}
+		if (r < 0) {
+			free(buffer);
+			return 1;
+		}
+		lastindex = 0;
+		for (i = 0; i < r; i++) {
+			curindex = i + len;
+			if (buffer[curindex] == '\n') {
+				if (isWrite) {
+		 			write(1, buffer + lastindex, curindex - lastindex + 1);
+					write(1, buffer + lastindex, curindex - lastindex + 1);
+					
+				} else {
+					isWrite = 1;
+				}
+				lastindex = curindex;
+			} 
+		}
+		if ((lastindex == 0) &&(curindex == k)) {
+			isWrite = 0;
+			len = 0;	
+		} else {
+			memmove(buffer, buffer + lastindex, curindex - lastindex);
+		}
+	}
+	free(buffer);
 }
