eeb73950b66bffdf7d33c8a764b04cbf61864afd
watson <watson@watson.(none)>
2013-04-24 13:57:50 +0400

    added watchthis.c with makefile

---
+++ simple-bash/watchthis.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdio.h>
+const char * newFile = "newFile.tmp";
+const char * oldFile = "oldFile.tmp";
+int main(int argc, char ** argv) {
+    int timeout = atoi(argv[1]);
+    printf("imhere\n");   
+    while (1)  {
+        int newfile = open(newFile, O_CREAT  | O_RDWR, S_IRWXU);
+        int pidexec = fork();
+        if (pidexec == 0) {
+            dup2(newfile, 1);
+            execvp(argv[2], &argv[2]);
+        } else { 
+            waitpid(pidexec, NULL, 0);
+        }
+        int pidcat = fork();
+        if (pidcat == 0) {
+           execlp("cat", "cat", newFile, NULL);
+        } else {
+            waitpid(pidcat, 0, NULL);
+        }
+        int piddiff = fork();
+        if (piddiff == 0) {
+           execlp("diff", "diff", "-u", newFile, oldFile, NULL);
+        } else {
+           waitpid(piddiff, 0, NULL);
+        }
+        int pidmv = fork();
+        if (pidmv == 0) {
+           execlp("mv", "mv", newFile, oldFile, NULL);
+        } else {
+           waitpid(pidmv, NULL, 0);
+        }
+        sleep(timeout); 
+    }
+}
