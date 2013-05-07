607c381de0be499407dabd0c2ad8feb3d3353dab
watson <watson@watson.(none)>
2013-05-07 17:11:59 +0400

    reading added

--- piperunner/runner.c
+++ piperunner/runner.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/stat.h>
-#include <vector.h>
+#include <vector>
+#include <stdlib.h>
 void  write_str (int  fd, char *str, int size) {
     int i = 0;
     while (i < size) {
@@ -32,15 +33,16 @@ int main (int argc , char ** argv) {
     int  fdin = open(filename, O_RDWR, S_IRWXU);
     int  output = 1;
 
-    char separator = "\0";
+    char separator = '\0';
     int curlen = 0;
     int myeof = 0;
     
-    int curind = 0;
     int curword = 0;
-    int lastsep = 0;
+    int issep = 0;
+    int iscreate = 1;
     std::vector<std::vector<std::vector<char> > > commands;
-    while (!myeof) {
+    int curind = -1;
+    while (1) {
         if (curlen == maxsize) {
             return 3;
         }
@@ -49,38 +51,60 @@ int main (int argc , char ** argv) {
         if (read_res < 0) {
            return -1;
         }
+        printf("imhere\n");
+        if (read_res == 0) {
+            break;
+        }
         int from = 0;
 
-        printf ("mhere\n");
         int pos = 0; 
 
-        for ( int i = 0; i < maxsize; i++) {
+        for ( int i = 0; i < read_res; i++) {
+            if (iscreate == 1) {
+                curind++;
+                curword = 0;
+                std::vector<std::vector<char > > tmp;
+                commands.push_back(tmp);
+                std::vector<char> tmp1;
+                commands[curind].push_back(tmp1);
+            }
             if ( buffer[i] == '\0' ) {
                 if (issep == 0) {
                     curword++;
                     std::vector<char> tmp1;
-                    commands[curind].push_bach(tmp1);
+                    commands[curind].push_back(tmp1);
+                    iscreate = 0;
+                    issep = 1;
                 } else {
-                    curind++;
-                    std::vector<std::vector<char > > tmp;
-                    commands[curind].push_back(tmp);
-                    std::vector<char> tmp1;
-                    commands[curind].push_bach(tmp1);
-                    curword = 0;
+                    iscreate = 1;
+                    issep = 0;
                 }
             } else {
                 commands[curind][curword].push_back(buffer[i]);
+                iscreate = 0;
+                issep = 0;
             }
         }
     }
+    printf("%i\n", curind);
+    
+
+
+
+
+
+
+
+
+
     int i, j, k;
-    for (i = 0; i < commands.size() - 1; i++) {
+    for (i = 0; i < commands.size() ; i++) {
         for (j = 0; j < commands[i].size() - 1; j++) {
-            for (k = 0; k < commands[i][j].size - 1; k++) {
+            for (k = 0; k < commands[i][j].size() ; k++) {
                 printf("%c\n", commands[i][j][k]);
             }
-            printf(" ");
+            printf("new word\n");
         }
-        printf("\n");
+        printf("new command\n");
     }
 }
