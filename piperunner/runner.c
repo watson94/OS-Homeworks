cf8d5fd648991791b67f737e5ac0411ac1622338
watson <watson@watson.(none)>
2013-05-07 21:47:52 +0400

    it works

--- piperunner/runner.c
+++ piperunner/runner.c
@@ -28,7 +28,7 @@ int findseparator(char separator, char * str, int size) {
 }
 
 char ** parsecommand(std::vector<std::vector<char > > com) {
-    char ** ans = (char **)malloc(com.size() + 1);
+    char ** ans = (char **) malloc((com.size() + 1) * sizeof(char*));
     for (int i = 0; i < com.size(); i++) {
         ans[i] = (char *) malloc(com[i].size() + 1);
         for (int j = 0; j < com[i].size(); j++) {
@@ -36,11 +36,16 @@ char ** parsecommand(std::vector<std::vector<char > > com) {
         }
         ans[i][com[i].size()] = 0;
     }
-    ans[com.size()] = NULL;
+    ans[com.size()] = 0;
     return ans;
 }
 
-
+void myfree(char ** arg, int size) {
+    for (int i = 0; i < size; i++) {
+        free(arg[i]);
+    }
+    free(arg);
+}
 
 int main (int argc , char ** argv) {
     const int maxsize = 4096;
@@ -68,16 +73,16 @@ int main (int argc , char ** argv) {
         if (read_res < 0) {
            return -1;
         }
-        printf("imhere\n");
         if (read_res == 0) {
             break;
         }
         int from = 0;
 
-        int pos = 0; 
-
         for ( int i = 0; i < read_res; i++) {
             if (iscreate == 1) {
+                if (curind >= 0) {
+                    commands[curind].pop_back();
+                }
                 curind++;
                 curword = 0;
                 std::vector<std::vector<char > > tmp;
@@ -104,37 +109,43 @@ int main (int argc , char ** argv) {
         }
     }
     
+    commands.pop_back();
 
     int curpipe[2];
     int lastpipe[2];
     pipe(lastpipe);
     pipe(curpipe);
     dup2(0, lastpipe[0]);
-//    for (int i = 0; i < commands.size() - 1; i++) {
+    int pp = 0;
+    for (int i = 0; i < commands.size() ; i++) {
         int pid = fork();
+        char ** mycommand = parsecommand(commands[i]);
         if (pid == 0) {
             dup2(lastpipe[0], 0);
-//            dup2(curpipe[1], 1);
-            char ** mycommand = parsecommand(commands[0]);
-            printf("%s\n", mycommand[0]);
-            printf("%s\n", mycommand[1]);
-            printf("%s\n", mycommand[2]);
+            close(lastpipe[0]);
+            close(lastpipe[1]);
+            if (i != commands.size() - 1) { 
+                dup2(curpipe[1], 1);
+
+            } 
+                
+            close(curpipe[0]);
+            close(curpipe[1]);
             execvp(mycommand[0], mycommand);
+            exit(1);
         } else {
-            waitpid(pid,NULL, 0);
+            if ( i == commands.size() - 1 ) {
+                waitpid(pid, NULL, 0);
+            }
+            close(curpipe[1]);
             lastpipe[0] = curpipe[0];
             lastpipe[1] = curpipe[1]; 
             pipe(curpipe);
+            myfree(mycommand, commands[i].size());
         }
-//    }
-
-
-
-
-
-
-
-/*    int i, j, k;
+    }
+/*
+    int i, j, k;
     for (i = 0; i < commands.size() ; i++) {
         for (j = 0; j < commands[i].size() - 1; j++) {
             for (k = 0; k < commands[i][j].size() ; k++) {
