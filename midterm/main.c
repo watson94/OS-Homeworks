04e8bad49110cc2e2ee1b8598468be3dbe8ecc6f
watson <watson@watson.(none)>
2013-05-09 16:56:02 +0400

    Remove unused parts of code

--- midterm/main.c
+++ midterm/main.c
@@ -12,17 +12,6 @@ void  write_str (int  fd, char *str, int size) {
     }
 }
 
-
-/*
-int findseparator(char separator, char * str, int size) {
-    int i;
-    for (i = 0; i < size; i++) {
-        if (str[i] == separator) {
-            return i;
-        }
-    }
-    return -1;
-} */
 int min (int a, int b) {
     if (a < b) return a;
     return b;
@@ -46,40 +35,4 @@ int main (int argc , char ** argv) {
         write_str(out, buffer, n);
     }
 
-
-/*    char separator = "\n";
-    int curlen = 0;
-    int myeof = 0;
-
-    while (!myeof) {
-        if (curlen == maxsize) {
-            return 3;
-        }
-
-        int read_res = read(filelist, buffer, maxsize - curlen + 1);
-        if (read_res < 0) {
-           return -1;
-        }
-        int from = curlen;
-        curlen += read_res;
-        if (read_res == 0) {
-            myeof = 1;
-            if (curlen == maxsize) {
-                return 3;
-            } else {
-                if (curlen > 0) {
-                    buffer[curlen] = '\n';
-                    write_str(output, buffer, curlen + 1);
-                }
-            }
-            break;
-        }
-        printf ("mhere\n");
-        int pos = 0; 
-        while ((pos = findseparator(separator, buffer + from, curlen - from)) >= 0 ) {
-            write_str(output, buffer, pos + 1);
-            memmove(buffer, buffer + pos + 1, curlen - pos);
-            curlen -= (pos + 1); 
-        }
-    }*/
 }
